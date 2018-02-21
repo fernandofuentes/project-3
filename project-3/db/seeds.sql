87343d82ea38b5d8c26bcba884b421db369cfd2d
brandongatlin <brandongatlin1981@me.com>
2018-02-20 20:58:27 -0600

    added schema and seed data for donor table

--- project-3/db/seeds.sql
+++ project-3/db/seeds.sql
@@ -4,3 +4,27 @@ INSERT INTO Volunteers (
 VALUES (
   "Smith", "Vinnie", "256-555-1234", "vinnie@mail.com", "1234 Main St., Houston, TX 77300", "Car"
 );
+
+
+
+INSERT INTO Donors (
+  business_name,
+  business_type,
+  phone_number,
+  email_address,
+  physical_address,
+  manager_name,
+  manager_phone_number,
+  comments
+)
+
+VALUES (
+  "Donnie's Pizzeria",
+  "Restaurant",
+  "(555) 555-1212",
+  "donnies@donnies.com",
+  "1 Leaf St. Conroe, TX 77304",
+  "Donnie Fanucci",
+  "(555) 555-7676",
+  "Their pizza is always a big hit, mama, mia."
+);
