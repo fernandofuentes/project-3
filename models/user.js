56cba2ba56489fe7dbe2e451a17ed52cbddbf0b5
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2018-03-06 21:01:08 -0600

    user js updates

--- models/user.js
+++ models/user.js
@@ -1,54 +1,49 @@
 // Set up database using Sequelize to take in user info
 
-module.exports = function (sequelize, DataTypes) {
-  const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    firstname: {
-      type: DataTypes.STRING,
-      notEmpty: true
-    },
-    lastname: {
-      type: DataTypes.STRING,
-      notEmpty: true
-    },
-    username: {
-      type: DataTypes.TEXT
-    },
-    email: {
-      type: DataTypes.STRING,
-      validate: {
-        isEmail: true
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    last_login: {
-      type: DataTypes.DATE
-    },
-    fav1: {
-      type: DataTypes.STRING
-    },
-    fav2: {
-      type: DataTypes.STRING
-    },
-    fav3: {
-      type: DataTypes.STRING
-    }
-  });
+module.exports = function(sequelize, DataTypes) {
+    const User2 = sequelize.define('User2', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        firstname: {
+            type: DataTypes.STRING,
+            notEmpty: true
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            notEmpty: true
+        },
+        username: {
+            type: DataTypes.TEXT
+        },
+        email: {
+            type: DataTypes.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_login: {
+            type: DataTypes.DATE
+        },
+        fav1: {
+            type: DataTypes.STRING
+        },
+        fav2: {
+            type: DataTypes.STRING
+        },
+        fav3: {
+            type: DataTypes.STRING
+        }
+    });
 
-  // Associating Users with Coins
-  User.associate = function (models) {
-    User.hasMany(models.Coins);
-  }
+    return User2;
 
-  return User;
-
-  document.getElementById("userName").innerHTML += User["id"];
+    document.getElementById("userName").innerHTML += User["id"];
 
 }
