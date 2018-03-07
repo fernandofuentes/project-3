6a3c25cd483fa0ebf8a2c6e335ee5430c8d7c442
Daniel Cannon <dancannon27@icloud.com>
2018-03-06 21:44:43 -0600

    donor updates to db

--- controllers/controller.js
+++ controllers/controller.js
@@ -34,4 +34,34 @@ router.post( "/sign-up/volunteer", function ( req, res ) {
     } );
 } );
 
+
+// POST route for saving a new donor
+router.post( "/sign-up/donor", function ( req, res ) {
+  console.log( "create Donor happened" );
+
+  db.Donor.create( {
+        business_name: req.body.business_name,
+        business_type: req.body.business_type,
+        phone_number: req.body.phone_number,
+        email_address: req.body.email_address,
+        physical_address: req.body.physical_address,
+        manager_name: req.body.manager_name,
+        manager_phone_number: req.body.manager_phone_number
+      } //end Donor.create
+      // , {
+      //   include:[{
+      //     association: buses.BusId
+      //   }]
+      // }
+    ).then( function ( dbFam ) {
+      res.json( dbFam );
+      console.log( ".then happened" );
+    } )
+    .catch( function ( err ) {
+      // Whenever a validation or flag fails, an error is thrown
+      // We can "catch" the error to prevent it from being "thrown", which could crash our node router
+      res.json( err );
+    } );
+} );
+
 module.exports = router;
