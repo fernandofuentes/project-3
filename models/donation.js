f42f247dbecd2303f16e77db6060dd9ba21464a8
brandongatlin <brandongatlin1981@me.com>
2018-03-08 17:56:45 -0600

    changed timestamps to true on all models

--- models/donation.js
+++ models/donation.js
@@ -1,46 +1,45 @@
-var Sequelize = require("sequelize");
+var Sequelize = require( "sequelize" );
 
-module.exports = function (sequelize, DataTypes) {
+module.exports = function ( sequelize, DataTypes ) {
 
-    var Donation = sequelize.define('Donation', {
-        food_item: {
-            allowNull: false,
-            type: DataTypes.STRING(50),
-            validate: {
-                len: [1, 50]
-            }
+  var Donation = sequelize.define( 'Donation', {
+    food_item: {
+      allowNull: false,
+      type: DataTypes.STRING( 50 ),
+      validate: {
+        len: [ 1, 50 ]
+      }
 
-        },
-        quantity: {
-            allowNull: false,
-            type: DataTypes.INTEGER(50),
-            validate: {
-                len: [1, 50]
-            }
-
-        },
-        donor_business_name: {
-            allowNull: false,
-            type: DataTypes.STRING(50),
-            validate: {
-                len: [1, 50]
-            }
-
-        }
     },
-    {
-      timestamps: false
-    });
+    quantity: {
+      allowNull: false,
+      type: DataTypes.INTEGER( 50 ),
+      validate: {
+        len: [ 1, 50 ]
+      }
 
-        // Donation.associate = function (models) {
-        //
-        //     Donation.belongsTo(models.donor, {
-        //         foreignKey: {
-        //             allowNull: true
-        //         },
-        //     });
-        // };
+    },
+    donor_business_name: {
+      allowNull: false,
+      type: DataTypes.STRING( 50 ),
+      validate: {
+        len: [ 1, 50 ]
+      }
 
-    return Donation;
+    }
+  }, {
+    timestamps: true
+  } );
+
+  // Donation.associate = function (models) {
+  //
+  //     Donation.belongsTo(models.donor, {
+  //         foreignKey: {
+  //             allowNull: true
+  //         },
+  //     });
+  // };
+
+  return Donation;
 
 };
