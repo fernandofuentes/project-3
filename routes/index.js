1acf509f06a398c7bf93bc89b6e00622edec4e1e
Will <wbattles87@yahoo.com>
2018-03-20 16:44:53 -0500

    working solo stuff

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { unregister } from './registerServiceWorker';
+
+
 const path = require( "path" );
 const router = require( "express" ).Router();
 const apiRoutes = require( "./api" );
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
 
 // API Routes
 router.use( "/api", apiRoutes );
