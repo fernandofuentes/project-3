b21b727a7689f91c329321be389ada0335990c50
Will <wbattles87@yahoo.com>
2018-03-22 04:33:06 -0500

    non working react but working about page

---
+++ public/js/component.js
@@ -0,0 +1,17 @@
+import React from "react";
+import React from "react-scripts";
+import react from "react-dom";
+
+class MyComponent extends React.Component {
+
+}
+
+Facts.propTypes = {
+
+};
+
+export default MyComponent;
+
+
+
+
--- views/about-us/script.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    setTimeout(function () {
-        ReactDOM.render(React.createElement(MyComponent, { context: $('body') }), document.getElementById('root'));
-    }, 0);
-});
-
